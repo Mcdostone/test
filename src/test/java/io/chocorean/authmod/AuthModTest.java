14e7898c06fe0bebacba9c3437dc43f59049dff4
Yann Prono <yann.prono@nist.gov>
2019-02-05 00:33:56 -0500

    test

--- src/test/java/io/chocorean/authmod/AuthModTest.java
+++ src/test/java/io/chocorean/authmod/AuthModTest.java
@@ -1,11 +1,13 @@
 package io.chocorean.authmod;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthModTest {
 
     @Test
     public void test() {
+        assertTrue(true, "Player should be logged");
     }
 
 }
